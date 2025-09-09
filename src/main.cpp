2474563b9bf60cbb9effca25ae0afb67f97bd32f
Szymon Kotuła <szymon.kotula@gmail.com>
2025-09-10 00:10:05 +0200

    c++ setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+extern "C" {
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/task.h"
+  #include "esp_log.h"
+}
+
+extern "C" void app_main(void) {
+  ESP_LOGI("MAIN","Hello from C++");
+  while (true) { 
+    vTaskDelay(pdMS_TO_TICKS(1000)); 
+}
+}   
