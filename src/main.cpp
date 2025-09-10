d92a7a054cae1672e8f5ce5b6e378efff5c5f147
Szymon Kotuła <szymon.kotula@gmail.com>
2025-09-10 14:38:45 +0200

    clang-format and references setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 extern "C" {
-  #include "freertos/FreeRTOS.h"
-  #include "freertos/task.h"
-  #include "esp_log.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 }
 
 extern "C" void app_main(void) {
-  ESP_LOGI("MAIN","Hello from C++");
-    while (true) { 
-        vTaskDelay(pdMS_TO_TICKS(1000)); 
-    }
-}   
+  ESP_LOGI("MAIN", "Hello from C++");
+  while (true) {
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
